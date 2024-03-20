6053c503a68af8b7602d636d324d2b0941f7c4cc
Robot Vinay <robotvinay.all@gmail.com>
2024-03-20 13:21:47 +0530

    Update weddingscript.js

--- weddingscript.js
+++ weddingscript.js
@@ -1,1 +1,12 @@
 
+ function setVideo() {
+            var videoSource = 'https://raw.githubusercontent.com/robotvinay/sampletest/main/harish_weds_mani_video_invitation.mp4';
+            var videoHTML = `
+                <video controls muted autoplay loop>
+                    <source src='${videoSource}' type='video/mp4'/>
+                </video>
+            `;
+            document.getElementById('wedding-video').innerHTML = videoHTML;
+        }
+
+setVideo();
