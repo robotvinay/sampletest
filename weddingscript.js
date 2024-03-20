02325689c61ed622527e1b3b14b611c7bb2bb00d
Robot Vinay <robotvinay.all@gmail.com>
2024-03-20 20:46:47 +0530

    Update weddingscript.js

--- weddingscript.js
+++ weddingscript.js
@@ -9,4 +9,20 @@
             document.getElementById('wedding-video').innerHTML = videoHTML;
         }
 
-setVideo();
+ function setIframeSize() {
+        var iframe = document.getElementById('youtube-iframe');
+        var container = document.getElementById('wedding-video');
+        var containerWidth = container.offsetWidth;
+
+        // Set different dimensions based on screen width breakpoints
+        if (window.innerWidth <= 600) {
+            iframe.style.width = '100%';
+            iframe.style.height = '200px';
+        } else if (window.innerWidth <= 1024) { 
+            iframe.style.width = '800px'; // Set your predefined width for large devices
+            iframe.style.height = '450px'; // Set your predefined height for large devices
+        } else {
+            iframe.style.width = '100%'; // Default width for other devices
+            iframe.style.height = '315px'; // Default height for other devices
+        }
+    }
